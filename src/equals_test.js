719a646413fbf6ba142ce7a4aec8c9cf1adc8587
Joe Dukes <joedukes.email@gmail.com>
2019-07-01 13:37:10 -0400

    Updated 'ws' version in package-lock.json

--- src/equals_test.js
+++ src/equals_test.js
@@ -1,9 +1,15 @@
-import { test } from 'tape-modern'
+import { test, assert } from 'tape-modern';
 
-import equals from './equals'
+import equals from './equals';
 
-test('equals function', async t => {
-  t.ok(equals(true, true))
-  t.notOk(equals(true, false))
+test('equals function', assert => {
+  // t.ok(equals(true, true))
+  // t.notOk(equals(true, false))
+  assert.equal(equals(1, 1), true, '1 === 1 is true');
 
-})
+  test('equals test 2'),
+    assert => {
+      const result = equals('hello', 'hello');
+      assert.equal(result, true, 'strings equal strings');
+    };
+});
